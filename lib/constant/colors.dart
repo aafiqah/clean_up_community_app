538b073c8537b4ec54059a8c3cd449f9e215f429
Alessa Afiqah <alessajohar@gmail.com>
2024-10-06 20:02:27 +0800

    add new colors in constant

---
+++ lib/constant/colors.dart
@@ -0,0 +1,29 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class CleanUpColor {
+  CleanUpColor._();
+  static const Color divider = Color(0xffD8D5D5);
+  static const Color icognitoBg = Color(0xff556080);
+  static const Color disabledText = Color(0xFF8B8B8B);
+  static const Color transparent = Color(0x00000000);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color black = Color(0xFF000000);
+
+  static const Color redLight = Color(0xFFFFA07A);
+  static const Color redMedium = Color(0xFFFF4500);
+  static const Color redDark = Color(0xFF8B0000);
+
+  static const Color blueLight = Color(0xFFADD8E6);
+  static const Color blueMedium = Color(0xFF1E90FF);
+  static const Color blueDark = Color(0xFF00008B);
+
+  static const Color greenLight = Color(0xFF90EE90);
+  static const Color greenMedium = Color(0xFF32CD32);
+  static const Color greenDark = Color(0xFF006400);
+
+  static const Color greyLight = Color(0xFFD3D3D3);
+  static const Color greyMedium = Color(0xFF808080);
+  static const Color greyDark = Color(0xFF404040);
+}
