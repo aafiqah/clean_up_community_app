f2b2fe0cff57119279e3dc0bf40692b4687cb92a
Alessa Afiqah <alessajohar@gmail.com>
2024-10-09 00:48:16 +0800

    add appbarBg asset

--- lib/constant/image_string.dart
+++ lib/constant/image_string.dart
@@ -2,4 +2,5 @@ class CleanUpImages {
   static const String logoCleanUp = "assets/images/logoCleanUp.png";
   static const String onBoarding1 = "assets/icons/onBoarding1.png";
   static const String onBoarding2 = "assets/icons/onBoarding2.png";
+  static const String appBarBg = "assets/icons/appbarBg.png";
 }
