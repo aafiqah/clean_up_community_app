b414f2192ddbec414c5fec1707dc1fc287ab4433
Alessa Afiqah <alessajohar@gmail.com>
2024-10-08 19:45:14 +0800

    add onBoarding assets

--- lib/constant/image_string.dart
+++ lib/constant/image_string.dart
@@ -1,3 +1,5 @@
-class CleanUpImages{
-   static const String logoCleanUp = "assets/images/logoCleanUp.png";
+class CleanUpImages {
+  static const String logoCleanUp = "assets/images/logoCleanUp.png";
+  static const String onBoarding1 = "assets/icons/onBoarding1.png";
+  static const String onBoarding2 = "assets/icons/onBoarding2.png";
 }
