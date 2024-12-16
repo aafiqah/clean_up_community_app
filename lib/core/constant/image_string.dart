16971f8e175ef50c35802c462a03fc6a7db05574
Alessa Afiqah <alessajohar@gmail.com>
2024-12-16 09:06:14 +0800

    add 2 assets for events details

--- lib/core/constant/image_string.dart
+++ lib/core/constant/image_string.dart
@@ -3,4 +3,6 @@ class CleanUpImages {
   static const String onBoarding1 = "assets/icons/onBoarding1.png";
   static const String onBoarding2 = "assets/icons/onBoarding2.png";
   static const String appBarBg = "assets/icons/appbarBg.png";
+  static const String calendar = "assets/icons/calendar.png";
+  static const String map = "assets/icons/map.png";
 }
