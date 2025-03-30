7962996e10289b1376d0737f009635da93009012
Alessa Afiqah <alessajohar@gmail.com>
2025-03-30 13:46:51 +0800

    feat home page

---
+++ lib/presentation/screen/clean_up/widget/upcoming_event_widget.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+
+import '../../../../core/index.dart';
+import '../../../widgets/index.dart';
+
+class UpcomingEventWidget extends StatelessWidget {
+  const UpcomingEventWidget({
+    super.key,
+    required this.eventName,
+    required this.eventDate,
+    required this.eventLocation,
+    this.imagePath,
+  });
+
+  final String eventName;
+  final String eventDate;
+  final String eventLocation;
+  final String? imagePath;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(8),
+      decoration: BoxDecoration(
+        color: CleanUpColor.white,
+        borderRadius: BorderRadius.circular(5),
+        boxShadow: const [
+          BoxShadow(
+            color: CleanUpColor.greyMedium,
+            offset: Offset(0.5, 0.5),
+            blurRadius: 2,
+          ),
+        ],
+      ),
+      child: Row(
+        children: [
+          Container(
+            width: MediaQuery.sizeOf(context).height / 9,
+            decoration: BoxDecoration(
+              image: imagePath != null
+                  ? DecorationImage(
+                      image: AssetImage(imagePath!),
+                      fit: BoxFit.cover,
+                    )
+                  : null,
+              color: CleanUpColor.greyLight,
+              borderRadius: BorderRadius.circular(10),
+            ),
+          ),
+          const SizedBox(width: 8),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  '$eventName - $eventDate',
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 2,
+                ),
+                const Spacer(),
+                Row(
+                  children: [
+                    Expanded(
+                      child: Text(
+                        eventLocation,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                    Submitbutton(
+                      onPressed: () {},
+                      text: 'Join',
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
