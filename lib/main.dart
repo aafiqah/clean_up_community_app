30e81faaf2f6d10ae3e4e0cfc12b68e2dcc2b650
Alessa Afiqah <alessajohar@gmail.com>
2025-05-25 11:38:24 +0800

    hide api credentials

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'core/config/firebase_options.dart';
 
 import 'core/index.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
