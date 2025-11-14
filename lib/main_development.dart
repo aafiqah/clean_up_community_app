c945a0f97b0cd5d695eb8d5bb3f5528cfe21b6b7
Alessa Afiqah <alessajohar@gmail.com>
2025-11-15 01:25:56 +0800

    refactor firebase in android and ios for each env

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,9 +1,20 @@
 import 'package:clean_up_community_app/app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:firebase_core/firebase_core.dart';
+
 import 'core/config/index.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  
+  // Load .env
+  await dotenv.load(fileName: ".env");
+
+  // Initialize Firebase
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   AppEnvironment.init(Environment.development);
 
